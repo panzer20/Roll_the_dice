b3b983ad0f07ef88fb266b103039c9ccaa2bf238
Hubert <hubert.jozefowski@wp.pl>
2025-03-17 16:50:26 +0100

    Update dice_visual.py
    Translating the file into English.

--- dice_visual.py
+++ dice_visual.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 
 from dice import Dice
 
-#Utworzenie dwóch kości do gry typu D6.
+#Creating two D6 dice.
 dice_1 = Dice()
 dice_2 = Dice(10)
 
-#Wykonanie pewnej liczby rzutów i umieszczenie wyników na liście.
+#Making a certain numbers of rolls and listing the results.
 results = [dice_1.roll() + dice_2.roll() for _ in range(50_000)]
 
-#Analiza wyników.
+#Analysis of results.
 max_result = dice_1.num_sides + dice_2.num_sides
 poss_results = range(2, max_result+1)
 frequencies = [results.count(value) for value in poss_results]
 
-#Wizualizacja wyników.
+#Results visualization.
 fig, ax = plt.subplots(figsize=(10,6))
 ax.bar(poss_results, frequencies, color='blue', edgecolor='black')
 
-#Zdefiniowanie tytułu wykresu i etykiet osi.
+#Defining the plot title and axis labels.
 ax.set_title("Wynik rzucania kośćmi D6 i D10 pięćdziesiąt tysięcy razy", fontsize=24)
 ax.set_xlabel("Wynik", fontsize=14)
 ax.set_ylabel("Częstotliwość występowania wartości", fontsize=14)
 
-#Zdefiniowanie wielkości etykiet.
+#Defining labels size.
 ax.tick_params(labelsize=14)
 
-#Ustawienie znaczników na osi X dla każdej wartości.
+#Set the markers on the X-axis for each value.
 ax.set_xticks(poss_results)
 
 fig.savefig('dice_visual.png')
